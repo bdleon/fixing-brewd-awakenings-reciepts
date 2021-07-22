6897211fc90d83604f3a8f0aac3d603452ac5824
Brandon De Leon <brandondeleon.615@gmail.com>
2021-07-22 11:47:08 -0500

    add an event listener on product

--- scripts/Products.js
+++ scripts/Products.js
@@ -1,12 +1,28 @@
 import { getProducts } from "./database.js"
 
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("product")) {
+            const [,price] = itemClicked.id.split("--")
+
+            for (const product of products) {
+                if (product.id === parseInt()) {
+                    window.alert(`${walker.name} services ${walker.city}`)
+                }
+            }
+        }
+    }
+)
+
 const products = getProducts()
 
 export const Products = () => {
-    let html = "<ul>`
+    let html = "<ul>"
 
     for (const product of products) {
-        html += `<li id="product--${product.id}">${product.title}</li>`
+        html += `<li id="product--${product.id}">${product.name}</li>`
     }
 
     html += "</ul>"
